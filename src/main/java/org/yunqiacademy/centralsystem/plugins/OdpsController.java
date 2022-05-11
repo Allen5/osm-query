34fdc7a6c7cbda40390d7493dd74d3245232efbd
三爷 <allenlikeu@gmail.com>
2022-05-11 13:32:34 +0800

    feat: fix code

--- src/main/java/org/yunqiacademy/centralsystem/plugins/OdpsController.java
+++ src/main/java/org/yunqiacademy/centralsystem/plugins/OdpsController.java
@@ -4,14 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.yunqiacademy.centralsystem.plugins.service.OdpsService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
-import java.util.Map;
 
 @Path("/api")
 @Slf4j
